51449a1f7a5614795a39dfb171f438808502e0b1
brainbeast <62809004+andy-dendy-repo@users.noreply.github.com>
2020-07-14 14:47:38 +0300

    Add files via upload

---
+++ Parser.cs
@@ -0,0 +1,69 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SitemapGenerator.Sitemap
+{
+    public static class Parser
+    {
+        public static IEnumerable<string> GetAHrefs(string text)
+        {
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(text);
+            var tags=document.DocumentNode.SelectNodes(".//*");
+            foreach(var tag in tags)
+            {
+                if (tag.Name == "a" )
+                {
+                    string href = tag.GetAttributeValue("href", string.Empty);
+                    if (!string.IsNullOrEmpty(href))
+                        yield return href;
+                }
+            }
+        }
+        public static void Normalize(string domain,string current,ref List<string> url)
+        {
+            url = url.Select(x => {
+            if (!x.EndsWith('/'))
+            {
+                if (x.Contains('/'))
+                {
+                    string end = x.Split('/').Last();
+                    if ((end.Contains(".") || end.Contains("#")) && 
+                        (!end.Contains(".html") && !end.Contains(".php")))
+                    {
+                            return string.Empty;
+                    }
+                }
+                    else
+                        if (x.Contains('.') || x.Contains("#"))
+                        return string.Empty;
+            }
+                if ((x.Contains("http://") || x.Contains("https://")) && !x.Contains(domain))
+                {
+                    return string.Empty;
+                }
+                if (!x.Contains("http://") && !x.Contains("https://"))
+                {
+                    if(x[0]=='/')
+                    {
+                        return "https://" + domain  + x;
+                    }
+                    else
+                    {
+                        if (x[0] == '#')
+                            return string.Empty;
+                        int last = current.LastIndexOf('/');
+                        string meta = current.Substring(0, last);
+                        return meta + "/" + x;
+                    }
+                }
+                return x;
+            }).ToList();
+
+            url = url.Where(x => !string.IsNullOrEmpty(x)).ToList();
+        }
+    }
+}
---
+++ Sitemapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitemapGenerator.Sitemap
+{
+    public class Sitemapper
+    {
+        public SitemapDocument Document;
+        public string BaseUrl { get; set; }
+        public string Domain { get; set; }
+        public string SavePath { get; set; }
+        public bool Exclude { get; set; }
+        private ILoader _loader = new Loader();
+        public delegate void Info();
+        public Info Notify;
+        public Sitemapper(string domain, string baseulr)
+        {
+            Domain = domain;
+            BaseUrl = baseulr.Contains("http://") || baseulr.Contains("https://") ? baseulr : ("https://"+baseulr);
+            SavePath = DateTime.Now.ToString("yyyymmddhhmmss")+".xml";
+            Exclude = true;
+            Document = new SitemapDocument();
+        }
+        public async Task GenerateSitemap()
+        {
+            List<string> new_urls = new List<string>();
+            List<string> visited = new List<string>();
+            Document.Urls = visited;
+            new_urls.Add(BaseUrl);
+            do
+            {
+                List<string> hrefs=new List<string>();
+                foreach (var url in new_urls)
+                {
+                    string text =await _loader.Get(url);
+                    if (string.IsNullOrEmpty(text)) continue;
+                    visited.Add(url);
+                    Notify?.Invoke();
+                    List<string> meta=Parser.GetAHrefs(text).Distinct().ToList();
+                    Parser.Normalize(Domain,url,ref meta);
+                    if (Exclude)
+                        meta = meta.Select(u => u.Contains('?') ? u.Split('?')[0] : u).ToList();
+                    hrefs.AddRange(meta);
+                    hrefs = hrefs.Distinct().ToList();
+                }
+                new_urls = hrefs.Except(visited).ToList();
+            }
+            while (new_urls.Count != 0);
+            Document.Save(SavePath);
+        }
+    }
+}
